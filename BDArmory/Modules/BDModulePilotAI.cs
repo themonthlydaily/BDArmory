53a9f86fd1653ccef02a089da32135dcff7fac8e
Brett Ryland <b.n.ryland@gmail.com>
2020-06-16 10:37:33 +0200

    Adjust 'interval' parameter in vessel-vessel collision prediction based on vessel speed.

--- BDArmory/Modules/BDModulePilotAI.cs
+++ BDArmory/Modules/BDModulePilotAI.cs
@@ -579,8 +579,8 @@ namespace BDArmory.Modules
                 return false;
             }
 
-            float time = Mathf.Min(0.5f, maxTime);
-            while (time < maxTime)
+            float time = Mathf.Min(interval, maxTime);
+            while (time <= maxTime)
             {
                 Vector3 tPos = AIUtils.PredictPosition(v, time);
                 Vector3 myPos = AIUtils.PredictPosition(vessel, time);
@@ -1399,7 +1399,7 @@ namespace BDArmory.Modules
                 {
                     if (vs.Current == null) continue;
                     if (vs.Current == vessel || vs.Current.Landed || !(Vector3.Dot(vs.Current.transform.position - vesselTransform.position, vesselTransform.up) > 0)) continue;
-                    if (!PredictCollisionWithVessel(vs.Current, 2.5f, 0.5f, out collisionAvoidDirection)) continue;
+                    if (!PredictCollisionWithVessel(vs.Current, 2.5f, 50.0f / Mathf.Max((float)vessel.srfSpeed, 100.0f), out collisionAvoidDirection)) continue; // Adjust "interval" parameter based on vessel speed.
                     if (vs.Current.FindPartModuleImplementing<IBDAIControl>()?.commandLeader?.vessel == vessel) continue;
                     vesselCollision = true;
                     break; // Early exit on first detected vessel collision. Chances of multiple vessel collisions are low.
