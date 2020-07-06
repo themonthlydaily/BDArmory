9cf8785b90ba416f47d43b1ee3af51418255263b
Cl0by <60481614+Cl0by@users.noreply.github.com>
2020-07-06 14:09:09 -0500

    updated vdata

--- BDArmory/Control/BDACompetitionMode.cs
+++ BDArmory/Control/BDACompetitionMode.cs
@@ -30,6 +30,7 @@ namespace BDArmory.Control
         public double lastPossibleRammingTime = -1;
         public Vessel rammedVessel;
         public Vessel rammingVessel;
+        public ScoringData otherVesselScoringData;
         public bool landedState;
         public double lastLandedTime;
         public double landerKillTimer;
@@ -42,7 +43,7 @@ namespace BDArmory.Control
         public HashSet<string> everyoneWhoHitMe = new HashSet<string>();
         public HashSet<string> everyoneWhoRammedMe = new HashSet<string>();
         public HashSet<string> everyoneWhoDamagedMe = new HashSet<string>();
-
+        
         public string LastPersonWhoDamagedMe()
         {
             //check if vessel got rammed
