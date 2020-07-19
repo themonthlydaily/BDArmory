8a3c1c31c0705e3de85f7324e8154ece6b5e2e44
Brett Ryland <b.n.ryland@gmail.com>
2020-07-19 23:46:05 +0200

    Avoid using Mathf.Pow for positive integer powers in ClosestTimeToCPA as it's slow. Extra check for rammingInformation being null in UpcomingCollisions in BDACompetitionMode.cs

--- BDArmory/Control/AIUtils.cs
+++ BDArmory/Control/AIUtils.cs
@@ -46,9 +46,9 @@ namespace BDArmory.Control
             {
                 return Mathf.Clamp(-Vector3.Dot(relPosition, relVelocity) / relVelocity.sqrMagnitude, 0f, maxTime);
             }
-            float D0 = Mathf.Pow(B, 2f) - 3f * A * C;
-            float D1 = 2 * Mathf.Pow(B, 3f) - 9f * A * B * C + 27f * Mathf.Pow(A, 2f) * D;
-            float E = Mathf.Pow(D1, 2f) - 4f * Mathf.Pow(D0, 3f); // = -27*A^2*discriminant
+            float D0 = B * B - 3f * A * C;
+            float D1 = 2 * B * B * B - 9f * A * B * C + 27f * A * A * D;
+            float E = D1 * D1 - 4f * D0 * D0 * D0; // = -27*A^2*discriminant
             // float discriminant = 18f * A * B * C * D - 4f * Mathf.Pow(B, 3f) * D + Mathf.Pow(B, 2f) * Mathf.Pow(C, 2f) - 4f * A * Mathf.Pow(C, 3f) - 27f * Mathf.Pow(A, 2f) * Mathf.Pow(D, 2f);
             if (E > 0)
             { // Single solution (E is positive)
@@ -61,7 +61,7 @@ namespace BDArmory.Control
             { // Triple solution (E is negative)
                 float F_real = D1 / 2f;
                 float F_imag = Mathf.Sign(D1) * Mathf.Sqrt(-E) / 2f;
-                float F_abs = Mathf.Sqrt(Mathf.Pow(F_real, 2f) + Mathf.Pow(F_imag, 2f));
+                float F_abs = Mathf.Sqrt(F_real * F_real + F_imag * F_imag);
                 float F_ang = Mathf.Atan2(F_imag, F_real);
                 float G_abs = Mathf.Pow(F_abs, 1f / 3f);
                 float G_ang = F_ang / 3f;
@@ -69,8 +69,8 @@ namespace BDArmory.Control
                 for (int i = 0; i < 3; ++i)
                 {
                     float G = G_abs * Mathf.Cos(G_ang + 2f * (float)i * Mathf.PI / 3f);
-                    float t = -1f / 3f / A * (B + G + D0 * G / Mathf.Pow(G_abs, 2f));
-                    if (t > 0f && Mathf.Sign(Vector3.Dot(relVelocity, relVelocity) + Vector3.Dot(relPosition, relAcceleration) + 3f * t * Vector3.Dot(relVelocity, relAcceleration) + 3f / 2f * Mathf.Pow(t, 2f) * Vector3.Dot(relAcceleration, relAcceleration)) > 0)
+                    float t = -1f / 3f / A * (B + G + D0 * G / G_abs / G_abs);
+                    if (t > 0f && Mathf.Sign(Vector3.Dot(relVelocity, relVelocity) + Vector3.Dot(relPosition, relAcceleration) + 3f * t * Vector3.Dot(relVelocity, relAcceleration) + 3f / 2f * t * t * Vector3.Dot(relAcceleration, relAcceleration)) > 0)
                     { // It's a minimum and in the future.
                         if (time < 0f || t < time) // Update the closest time.
                             time = t;
@@ -80,16 +80,17 @@ namespace BDArmory.Control
             }
             else
             { // Repeated root
-                if (Mathf.Abs(Mathf.Pow(B, 2) - 2f * A * C) < 1e-7)
+                if (Mathf.Abs(B * B - 2f * A * C) < 1e-7)
                 { // A triple-root.
                     return Mathf.Clamp(-B / 3f / A, 0f, maxTime);
                 }
                 else
                 { // Double root and simple root.
-                    return Mathf.Clamp(Mathf.Max((9f * A * D - B * C) / 2 / (Mathf.Pow(B, 2f) - 3f * A * C), (4f * A * B * C - 9f * Mathf.Pow(A, 2f) * D - Mathf.Pow(B, 3f)) / A / (Mathf.Pow(B, 2f) - 3f * A * C)), 0f, maxTime);
+                    return Mathf.Clamp(Mathf.Max((9f * A * D - B * C) / 2 / (B * B - 3f * A * C), (4f * A * B * C - 9f * A * A * D - B * B * B) / A / (B * B - 3f * A * C)), 0f, maxTime);
                 }
             }
         }
+
 
 
         /// <summary>
