0ae37ffad667626d78d8ac33ac8c4ef6a8aebe62
Brett Ryland <b.n.ryland@gmail.com>
2020-07-31 00:16:23 +0200

    Order the scores in the vessel switcher window (hit score, missile score, ram score) for dead planes to be consistent with alive planes

--- BDArmory/UI/LoadedVesselSwitcher.cs
+++ BDArmory/UI/LoadedVesselSwitcher.cs
@@ -625,10 +625,10 @@ namespace BDArmory.UI
                 {
                     // DEAD <death order>: vesselName(<Score>[, <RammingScore>])[ KILLED|RAMMED BY <otherVesselName>], where <Score> is the number of hits made  <RammingScore> is the number of parts destroyed.
                     statusString += "DEAD " + BDACompetitionMode.Instance.DeathOrder[key] + " : " + key + " (" + BDACompetitionMode.Instance.Scores[key].Score.ToString();
-                    if (BDACompetitionMode.Instance.Scores[key].totalDamagedPartsDueToRamming > 0)
-                        statusString += ", " + BDACompetitionMode.Instance.Scores[key].totalDamagedPartsDueToRamming;
                     if (BDACompetitionMode.Instance.Scores[key].totalDamagedPartsDueToMissiles > 0)
                         statusString += ", " + BDACompetitionMode.Instance.Scores[key].totalDamagedPartsDueToMissiles;
+                    if (BDACompetitionMode.Instance.Scores[key].totalDamagedPartsDueToRamming > 0)
+                        statusString += ", " + BDACompetitionMode.Instance.Scores[key].totalDamagedPartsDueToRamming;
                     switch (BDACompetitionMode.Instance.Scores[key].LastDamageWasFrom())
                     {
                         case DamageFrom.Bullet:
