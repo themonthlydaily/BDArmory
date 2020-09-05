315d9e3a0c409768cfbc050fb5273aa5aae5e48a
Brett Ryland <b.n.ryland@gmail.com>
2020-09-06 00:53:00 +0200

    Debris clean-up delay + slider

--- BDArmory/Control/BDACompetitionMode.cs
+++ BDArmory/Control/BDACompetitionMode.cs
@@ -360,6 +360,7 @@ namespace BDArmory.Control
             GameEvents.onVesselPartCountChanged.Remove(CheckVesselTypePartCountChanged);
             GameEvents.onNewVesselCreated.Remove(CheckVesselTypeNewVesselCreated);
             GameEvents.onVesselCreate.Remove(CheckVesselTypeVesselCreate);
+            GameEvents.onVesselCreate.Remove(DebrisDelayedCleanUp);
             rammingInformation = null; // Reset the ramming information.
         }
 
@@ -372,6 +373,7 @@ namespace BDArmory.Control
             GameEvents.onVesselPartCountChanged.Add(CheckVesselTypePartCountChanged);
             GameEvents.onNewVesselCreated.Add(CheckVesselTypeNewVesselCreated);
             GameEvents.onVesselCreate.Add(CheckVesselTypeVesselCreate);
+            GameEvents.onVesselCreate.Add(DebrisDelayedCleanUp);
             competitionStartTime = Planetarium.GetUniversalTime();
             lastTagUpdateTime = competitionStartTime;
         }
@@ -690,6 +692,7 @@ namespace BDArmory.Control
                 vessel.vesselType = VesselType.Plane;
                 message += ", changing vessel name and type to " + vessel.vesselName + ", " + vessel.vesselType;
                 Debug.Log("[BDACompetitionMode]: " + message);
+                return;
             }
         }
         #endregion
@@ -1196,6 +1199,7 @@ namespace BDArmory.Control
             var debrisToKill = new List<Vessel>();
             foreach (var vessel in FlightGlobals.Vessels)
             {
+                if (vessel.vesselType == VesselType.Debris) continue; // Handled by DebrisDelayedCleanUp
                 bool activePilot = false;
                 if (BDArmorySettings.RUNWAY_PROJECT && vessel.GetName() == "Pinata")
                 {
@@ -1258,6 +1262,29 @@ namespace BDArmory.Control
             }
         }
 
+        void DebrisDelayedCleanUp(Vessel debris)
+        {
+            if (debris.vesselType == VesselType.Debris)
+                StartCoroutine(DebrisDelayedCleanupCoroutine(debris, BDArmorySettings.DEBRIS_CLEANUP_DELAY));
+        }
+
+        private IEnumerator DebrisDelayedCleanupCoroutine(Vessel debris, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            if (debris != null)
+            {
+                Debug.Log("[RemoveObjectsDebris] " + debris.GetName());
+                debris.Die();
+            }
+            yield return new WaitForFixedUpdate();
+            if (debris != null)
+            {
+                var partsToKill = debris.parts;
+                foreach (var part in partsToKill)
+                    if (part != null)
+                        part.Die();
+            }
+        }
 
         // This is called every Time.fixedDeltaTime.
         void FixedUpdate()
