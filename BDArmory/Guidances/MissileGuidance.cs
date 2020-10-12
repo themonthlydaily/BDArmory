b4ec954bb81b42732f0b1d30950a5250d4054026
josuenos <josuenos@users.noreply.github.com>
2020-10-12 13:06:17 -0400

    Correct comment

--- BDArmory/Guidances/MissileGuidance.cs
+++ BDArmory/Guidances/MissileGuidance.cs
@@ -153,7 +153,7 @@ namespace BDArmory.Guidances
             // Calculate time to CPA to determine target position
             float timeToCPA = missileVessel.ClosestTimeToCPA(targetPosition, targetVelocity, targetAcceleration, 16f);
             timeToImpact = (timeToCPA < 16f) ? timeToCPA : timeToImpact;
-            // Ease in velocity from 16s to 8s, ease in acceleration from 10s to 1s using the logistic function to give smooth adjustments to target point.
+            // Ease in velocity from 16s to 8s, ease in acceleration from 8s to 2s using the logistic function to give smooth adjustments to target point.
             float easeAccel = Mathf.Clamp01(1.1f / (1f + Mathf.Exp((timeToCPA - 5f))) - 0.05f);
             float easeVel = Mathf.Clamp01(2f - timeToCPA / 8f);
             return AIUtils.PredictPosition(targetPosition, targetVelocity * easeVel, targetAcceleration * easeAccel, timeToCPA);
