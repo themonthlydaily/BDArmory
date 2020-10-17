df426773ce7a2b5fb4761a11f5a244c0b6fb1484
SuicidalInsanity <origamimage@gmail.com>
2020-10-17 16:12:27 -0700

    Fixing conflicts

--- BDArmory/UI/BDArmorySetup.cs
+++ BDArmory/UI/BDArmorySetup.cs
@@ -721,7 +721,7 @@ namespace BDArmory.UI
                 if (GUI.Button(new Rect(toolWindowWidth - _windowMargin - 3 * _buttonSize, _windowMargin, _buttonSize, _buttonSize), "Sp", vsStyle))
                 {
                     showVesselSpawnerGUI = !showVesselSpawnerGUI;
-                    if(!showVesselSpawnerGUI)
+                    if (!showVesselSpawnerGUI)
                         SaveConfig();
                 }
             }
