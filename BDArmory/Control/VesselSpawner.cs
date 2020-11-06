dffbe88e212ddf986f790bfc4432c8a9c079800b
Brett Ryland <b.n.ryland@gmail.com>
2020-11-06 17:12:35 +0100

    Fix a potential NRE in continuous spawning where a spawned vessel gets destroyed before it's activated.

--- BDArmory/Control/VesselSpawner.cs
+++ BDArmory/Control/VesselSpawner.cs
@@ -905,6 +905,7 @@ namespace BDArmory.Control
                     // Fix control point orientation by setting the reference transformations to that of the root parts and re-orient the vessels accordingly.
                     foreach (var vessel in vesselsToActivate)
                     {
+                        if (vessel == null) continue; // In case the vessel got destroyed in the mean time.
                         if (BDArmorySettings.DRAW_DEBUG_LABELS) Debug.Log("[VesselSpawner]: " + vessel.vesselName + " setting reference transform to that of: " + vessel.rootPart + " refT: " + vessel.ReferenceTransform + " from " + vessel.GetReferenceTransformPart());
                         vessel.SetReferenceTransform(vessel.rootPart);
                         vessel.SetRotation(Quaternion.FromToRotation(-vessel.ReferenceTransform.up, -geeDirection) * vessel.transform.rotation); // Re-orient the vessel to the local gravity direction.
