a439b4c952fc00d53a424dc9a318b95b30730c95
SuicidalInsanity <origamimage@gmail.com>
2021-03-11 15:51:31 -0800

    NRE fixes

--- BDArmory/FX/FireFX.cs
+++ BDArmory/FX/FireFX.cs
@@ -37,6 +37,11 @@ namespace BDArmory.FX
         KSPParticleEmitter[] pEmitters;
         void OnEnable()
         {
+            if (parentPart == null)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
             hasFuel = true;
             startTime = Time.time;
             foreach (var existingLeakFX in parentPart.GetComponentsInChildren<FuelLeakFX>())
@@ -58,7 +63,7 @@ namespace BDArmory.FX
             {
                 burnTime = 10;
             }
-            if (parentPart.parent.protoModuleCrew.Count > 0)
+            if (parentPart.parent != null && parentPart.parent.protoModuleCrew.Count > 0)
             {
                 burnTime = 20; //though ajdacent parts will take longer to get to and extingusih
             }
@@ -73,6 +78,7 @@ namespace BDArmory.FX
                 }
             }
         }
+
         void onDisable()
         {
             BDArmorySetup.numberOfParticleEmitters--;
@@ -83,6 +89,7 @@ namespace BDArmory.FX
                     EffectBehaviour.RemoveParticleEmitter(pe);
                 }
         }
+
         void Update()
         {
             if (!gameObject.activeInHierarchy || !HighLogic.LoadedSceneIsFlight)
@@ -220,6 +227,7 @@ namespace BDArmory.FX
             }
             ////////////////////////////////////////////
         }
+
         void Detonate()
         {
             if (!parentPart.partName.Contains("exploding"))
@@ -300,6 +308,7 @@ namespace BDArmory.FX
                 gameObject.SetActive(false);
             }
         }
+
         public void AttachAt(Part hitPart, RaycastHit hit, Vector3 offset, string sourcevessel, float burnTime = -1)
         {
             parentPart = hitPart;
@@ -311,6 +320,7 @@ namespace BDArmory.FX
             SourceVessel = sourcevessel;
             gameObject.SetActive(true);
         }
+
         public void OnParentDestroy()
         {
             if (parentPart)
@@ -323,6 +333,7 @@ namespace BDArmory.FX
                 gameObject.SetActive(false);
             }
         }
+
         public void OnDestroy()
         {
             OnParentDestroy();
