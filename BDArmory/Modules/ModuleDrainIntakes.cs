f41524ddb11f006a5999bf4d63bbb337f3fc92d1
SuicidalInsanity <origamimage@gmail.com>
2021-03-10 23:02:30 -0800

    RWPS3R2 Chernobyl code beta

---
+++ BDArmory/Modules/ModuleDrainIntakes.cs
@@ -0,0 +1,39 @@
+﻿using BDArmory.FX;
+using BDArmory.UI;
+using System.Linq;
+using UnityEngine;
+
+namespace BDArmory.Modules
+{
+    public class ModuleDrainIntakes : PartModule
+    {
+        public float drainRate = 999;
+        public float drainDuration = 20;
+        public override void OnStart(StartState state)
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                part.force_activate();
+            }
+            base.OnStart(state);
+        }
+
+        public void Update()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                drainDuration -= Time.deltaTime;
+                if (drainDuration > 0)
+                {
+                    part.RequestResource("IntakeAir", (double)(drainRate * Time.deltaTime), ResourceFlowMode.ALL_VESSEL);
+                    part.RequestResource("IntakeAtm", (double)(drainRate * Time.deltaTime), ResourceFlowMode.ALL_VESSEL);
+                }
+                else
+                {
+                    part.RemoveModule(this);
+                }
+            }
+        }
+    }
+}
+
