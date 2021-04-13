4742535f8c89889bba6f0076ea1562a412c1daf3
josuenos <josuenos@users.noreply.github.com>
2021-04-13 15:35:43 -0400

    Revert regain energy useAB change.

--- BDArmory/Modules/BDModulePilotAI.cs
+++ BDArmory/Modules/BDModulePilotAI.cs
@@ -1330,9 +1330,9 @@ namespace BDArmory.Modules
             targetDirection = Vector3.RotateTowards(vessel.Velocity(), targetDirection, 15f * Mathf.Deg2Rad, 0).normalized;
 
             if (throttleOverride >= 0)
-                AdjustThrottle(maxSpeed, false, useAB, throttleOverride);
+                AdjustThrottle(maxSpeed, false, true, throttleOverride);
             else
-                AdjustThrottle(maxSpeed, false, useAB);
+                AdjustThrottle(maxSpeed, false, true);
 
             FlyToPosition(s, vesselTransform.position + (targetDirection * 100), true);
         }
