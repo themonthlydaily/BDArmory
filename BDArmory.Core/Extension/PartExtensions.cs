3dae746e40bb4319c516c4245188d5e8603b73fd
SuicidalInsanity <origamimage@gmail.com>
2021-04-23 17:52:41 -0700

    fix DR math

--- BDArmory.Core/Extension/PartExtensions.cs
+++ BDArmory.Core/Extension/PartExtensions.cs
@@ -416,10 +416,9 @@ namespace BDArmory.Core.Extension
                     {
                         Debug.Log("[BDArmory.PartExtensions]: Damage Before Reduction : " + damage);
                         Debug.Log("[BDArmory.PartExtensions]: Damage Reduction (%) : " + 1 + (((strength * (density / 1000)) * armor) / 1000000));
-                        Debug.Log("[BDArmory.PartExtensions]: Damage After Armor : " + (damage *= 1 + (((strength * (density / 1000)) * armor) / 1000000)));
+                        Debug.Log("[BDArmory.PartExtensions]: Damage After Armor : " + (damage /= 1 + (((strength * (density / 1000)) * armor) / 1000000)));
                     }
-                    damage *= 1 + (((strength * (density / 1000)) * armor) / 1000000); //500mm of DU yields about 95% reduction, 500mm steel = 80% reduction, Aramid = 73% reduction
-                    //still need to look into spalling implementation for explosions
+                    damage /= 1 + (((strength * (density / 1000)) * armor) / 1000000); //500mm of DU yields about 95% reduction, 500mm steel = 80% reduction, Aramid = 73% reduction
                     break;
                 default:
                     if (!(penetrationfactor >= 1f))
