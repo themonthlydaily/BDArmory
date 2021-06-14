d50864b7530e8e0fc27a713fb854fb8196aa21a9
SuicidalInsanity <origamimage@gmail.com>
2021-06-14 13:49:43 -0700

    Add vesselname to pilot kill message

--- BDArmory/Misc/BattleDamageHandler.cs
+++ BDArmory/Misc/BattleDamageHandler.cs
@@ -407,7 +407,7 @@ namespace BDArmory.Misc
                                 crewMember.StartRespawnPeriod();
                             }
                             //ScreenMessages.PostScreenMessage(crewMember.name + " killed by damage to " + part.vessel.name + part.partName + ".", 5.0f, ScreenMessageStyle.UPPER_LEFT);
-                            ScreenMessages.PostScreenMessage("Cockpit snipe! " + crewMember.name + " killed!", 5.0f, ScreenMessageStyle.UPPER_LEFT);
+                             ScreenMessages.PostScreenMessage("Cockpit snipe on " + part.vessel.GetName() + "! " + crewMember.name + " killed!", 5.0f, ScreenMessageStyle.UPPER_LEFT);
                         }
                     }
                 }
