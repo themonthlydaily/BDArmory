34124698640b9037369c3a47079b9d37aa866608
SuicidalInsanity <origamimage@gmail.com>
2021-11-14 13:39:27 -0800

    Add persistant GUI...
    option to Vessel Switcher GUI when UI toggled off

--- BDArmory.Core/BDArmorySettings.cs
+++ BDArmory.Core/BDArmorySettings.cs
@@ -29,6 +29,7 @@ namespace BDArmory.Core
         [BDAPersistentSettingsField] public static float VESSEL_SWITCHER_WINDOW_WIDTH = 500f;
         [BDAPersistentSettingsField] public static bool VESSEL_SWITCHER_WINDOW_SORTING = false;
         [BDAPersistentSettingsField] public static bool VESSEL_SWITCHER_WINDOW_OLD_DISPLAY_STYLE = false;
+        [BDAPersistentSettingsField] public static bool VESSEL_SWITCHER_PERSIST_UI = false;
         [BDAPersistentSettingsField] public static float VESSEL_SPAWNER_WINDOW_WIDTH = 480f;
         [BDAPersistentSettingsField] public static float EVOLUTION_WINDOW_WIDTH = 350f;
 
