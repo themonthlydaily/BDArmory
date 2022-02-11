185cfcfd150fe6f7d240fbddcf19a3ed9e89f963
Brett Ryland <b.n.ryland@gmail.com>
2022-02-11 16:57:40 +0100

    Add python script that does the optimise-vessels-per-heat calculaton.

---
+++ _Other Stuff/optimise_vessels_per_heat.py
@@ -0,0 +1,24 @@
+import argparse
+from typing import Tuple
+
+
+def OptimiseVesselsPerHeat(count: int, limits: Tuple[int, int] = (6, 10)) -> Tuple[int, int]:
+    options = (8, 7, 6, 5) if count < 11 else reversed(list(range(limits[0], limits[1] + 1)))
+    for val in options:
+        if count % val == 0:
+            return val, 0
+    result = OptimiseVesselsPerHeat(count + 1, limits)
+    return result[0], result[1] + 1
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Optimise the number of vessels per heat.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("count", type=int, help="The number of craft in the tournament.")
+    parser.add_argument("-l", "--limits", type=int, nargs=2, default=(6, 10), help="Limits on the number of vessels.")
+    args = parser.parse_args()
+
+    result = OptimiseVesselsPerHeat(args.count, args.limits)
+    string = f"Optimal is {args.count//result[0]} heats with {result[0]} vessels"
+    if result[1] > 0:
+        string += f" and {result[1]} heat{'s' if result[1]>1 else ''} with {result[0]-1} vessels"
+    print(string)
