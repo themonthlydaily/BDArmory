ffa37630109d4b296f805ec0560ff1e320214091
Brett Ryland <b.n.ryland@gmail.com>
2022-02-24 00:25:00 +0100

    Add option to save the summary plot to a PNG file.

--- BDArmory/Distribution/GameData/BDArmory/plot_summary.py
+++ BDArmory/Distribution/GameData/BDArmory/plot_summary.py
@@ -4,6 +4,7 @@
 import argparse
 import csv
 import sys
+import tempfile
 from pathlib import Path
 
 # Third party imports
@@ -15,6 +16,7 @@ VERSION = "1.2"
 parser = argparse.ArgumentParser(description="Plot the scores of a tournament as they accumulated per round", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("tournament", nargs="?", type=str, help="The tournament to plot (optional).")
 parser.add_argument('-t', '--title', type=str, help="A title.")
+parser.add_argument('-s', '--save', action='store_true', help="Save a PNG image instead of displaying the graph.")
 parser.add_argument("--version", action='store_true', help="Show the script version, then exit.")
 args = parser.parse_args()
 
@@ -33,9 +35,14 @@ with open(tournamentDir / "summary.csv", 'r') as f:
 vessel_count = data.index([]) - 1
 names = [data[row][0] for row in range(len(data) - vessel_count, len(data))]
 scores = numpy.array([[float(v) for v in data[row][1:]] for row in range(len(data) - vessel_count, len(data))])
-plt.figure(figsize=(8, 5), dpi=200)
+plt.figure(figsize=(16, 10), dpi=200)
 plt.plot(scores.transpose(), linewidth=5)
-plt.legend(names)
+plt.legend(names, loc='upper left')
 if args.title is not None:
     plt.title(args.title)
-plt.show(block=True)
+if args.save:
+    fd, filename = tempfile.mkstemp(suffix='.png')
+    plt.savefig(filename, dpi='figure', bbox_inches='tight')
+    print(f"Image saved to {filename}")
+else:
+    plt.show(block=True)
