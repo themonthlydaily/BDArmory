2fb5cd2fae5ccd4af7064e87d198e016c62e787f
Brett Ryland <b.n.ryland@gmail.com>
2022-03-24 12:37:11 +0100

    Change the raise/lower gear altitude to min(50, minAlt)

--- BDArmory/Modules/BDModulePilotAI.cs
+++ BDArmory/Modules/BDModulePilotAI.cs
@@ -2333,7 +2333,7 @@ namespace BDArmory.Modules
         {
             if (!vessel.LandedOrSplashed)
             {
-                if (vessel.radarAltitude > 25.0f)
+                if (vessel.radarAltitude > Mathf.Min(50f, minAltitude / 2f))
                     vessel.ActionGroups.SetGroup(KSPActionGroup.Gear, false);
                 else
                     vessel.ActionGroups.SetGroup(KSPActionGroup.Gear, true);
