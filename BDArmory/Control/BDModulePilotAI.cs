3cd56e9ceef3acdfbcf593b69992e064b7eaae35
Brett Ryland <b.n.ryland@gmail.com>
2022-07-03 20:49:45 +0200

    Set the right timeScale variable.

--- BDArmory/Control/BDModulePilotAI.cs
+++ BDArmory/Control/BDModulePilotAI.cs
@@ -1306,7 +1306,7 @@ namespace BDArmory.Control
             if (!autoTune)
             {
                 pidAutoTuning.RevertPIDValues(true);
-                if (BDArmorySettings.TIME_OVERRIDE) { BDArmorySettings.TIME_OVERRIDE = false; BDArmorySettings.TIME_SCALE = 1f; }
+                if (BDArmorySettings.TIME_OVERRIDE) { BDArmorySettings.TIME_OVERRIDE = false; Time.timeScale = 1f; }
             }
             pidAutoTuning.ResetMeasurements();
             Fields["autoTuningLossLabel"].guiActive = autoTune;
@@ -4118,6 +4118,7 @@ namespace BDArmory.Control
                         AI.autoTune = false;
                         RevertPIDValues(true); // Revert to the best settings.
                         AI.StoreSettings(); // Store the current settings for recall in the SPH.
+                        GUIUtils.RefreshAssociatedWindows(AI.part);
                     }
                     optimiser.Update();
                     AI.autoTuningLossLabel = $"{loss:G6}";
