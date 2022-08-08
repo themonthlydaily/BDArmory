0a9eb732a30a4143ea6ae14650126661746cf551
BillNyeTheIE <BillNyeTheIlluminatiEye123@protonmail.com>
2022-08-08 10:01:56 -0700

    Updated ChangeLog to reflect new changes and swapped the debug to DEBUG_ARMOR

--- BDArmory/Ammo/PooledBullet.cs
+++ BDArmory/Ammo/PooledBullet.cs
@@ -883,7 +883,7 @@ namespace BDArmory.Bullets
                     //penetration = ProjectileUtils.CalculatePenetration(caliber, newCaliber, bulletMass, impactSpeed, Ductility, Density, Strength, thickness, apBulletMod, sabot);
                     penetration = ProjectileUtils.CalculatePenetration(caliber, impactSpeed, bulletMass, apBulletMod, Strength, vFactor, muParam1, muParam2, muParam3, sabot, length);
                 
-                if (BDArmorySettings.DEBUG_WEAPONS)
+                if (BDArmorySettings.DEBUG_ARMOR)
                 {
                     Debug.Log("[BDArmory.PooledBullet] Penetration: " + penetration + "mm. impactSpeed: " + impactSpeed + "m/s. bulletMass = " + bulletMass + "kg. Caliber: " + caliber + "mm. Length: " + length + "mm. Sabot: " + sabot);
                 }
