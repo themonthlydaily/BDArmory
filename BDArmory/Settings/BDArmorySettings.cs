577ae0e594cce48a1b3230e36f483f5aa7a911a3
Brett Ryland <b.n.ryland@gmail.com>
2022-09-04 00:40:01 +0200

    Add a building damage multiplier. Fix the bug preventing explosive damage applying to buildings.

--- BDArmory/Settings/BDArmorySettings.cs
+++ BDArmory/Settings/BDArmorySettings.cs
@@ -143,6 +143,7 @@ namespace BDArmory.Settings
         [BDAPersistentSettingsField] public static float EXP_DMG_MOD_ROCKET = 1f;               // Rocket explosion damage multiplier (FIXME needs tuning; Note: rockets used Ballistic mod before, but probably ought to be more like missiles)
         [BDAPersistentSettingsField] public static float EXP_DMG_MOD_BATTLE_DAMAGE = 1f;        // Battle damage explosion damage multiplier (FIXME needs tuning; Note: CASE-0 explosions used Missile mod, while CASE-1, CASE-2 and fuel explosions used Ballistic mod)
         [BDAPersistentSettingsField] public static float EXP_IMP_MOD = 0.25f;
+        [BDAPersistentSettingsField] public static float BUILDING_DMG_MULTIPLIER = 1f;
         [BDAPersistentSettingsField] public static bool EXTRA_DAMAGE_SLIDERS = false;
         [BDAPersistentSettingsField] public static float WEAPON_FX_DURATION = 15;               //how long do weapon secondary effects(EMP/choker/gravitic/etc) last
         [BDAPersistentSettingsField] public static float ZOMBIE_DMG_MULT = 0.1f;
