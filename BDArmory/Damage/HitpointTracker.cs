8011089cb6d84ec1c5dfe3b683d6db9355b6a5e6
Brett Ryland <b.n.ryland@gmail.com>
2022-11-11 00:22:27 +0100

    Fix issue with _hullConfigured and _hpConfigured not being set true within a reasonable time for certain parts.

--- BDArmory/Damage/HitpointTracker.cs
+++ BDArmory/Damage/HitpointTracker.cs
@@ -1450,7 +1450,11 @@ namespace BDArmory.Damage
         }
         void SetHullMass()
         {
-            if (IgnoreForArmorSetup) return;
+            if (IgnoreForArmorSetup)
+            {
+                _hullConfigured = true;
+                return;
+            }
             if (isAI || ArmorPanel || BDArmorySettings.RESET_HULL || BDArmorySettings.LEGACY_ARMOR) HullTypeNum = HullInfo.materials.FindIndex(t => t.name == "Aluminium");
 
             var OldHullMassAdjust = HullMassAdjust;
