8b0563cd6896a51bc602b6ec6736f63e4200ba97
Brett Ryland <b.n.ryland@gmail.com>
2022-11-15 12:57:43 +0100

    Add a --tsv option to the n-choose-k parsing script to output a TSV file instead of a CSV file.

--- BDArmory/Distribution/GameData/BDArmory/parse_n-choose-k_results.py
+++ BDArmory/Distribution/GameData/BDArmory/parse_n-choose-k_results.py
@@ -7,11 +7,12 @@ import sys
 from collections import Counter
 from pathlib import Path
 
-VERSION = "1.0"
+VERSION = "1.1"
 
 parser = argparse.ArgumentParser(description="Parse results.json of a N-choose-K style tournament producing a table of who-beat-who.", formatter_class=argparse.ArgumentDefaultsHelpFormatter, epilog="Note: this also works on FFA style tournaments, but may not be meaningful.")
 parser.add_argument('results', type=str, nargs='?', help="results.json file to parse.")
 parser.add_argument('-o', '--output', default="n-choose-k.csv", help="File to output CSV to.")
+parser.add_argument('--tsv', action='store_true', help="Output to a TSV (tab-separated values) file instead of a CSV file.")
 parser.add_argument("--version", action='store_true', help="Show the script version, then exit.")
 args = parser.parse_args()
 
@@ -28,7 +29,6 @@ if args.results is None:
         if len(tournamentFolders) > 0:
             args.results = tournamentFolders[-1] / "results.json"  # Results in latest tournament dir
 
-
 if args.results is not None:
     results_file = Path(args.results)
     if not results_file.exists():
@@ -44,9 +44,13 @@ if args.results is not None:
         t = [[0] * len(names) for i in range(len(names))]
         for k, c in counts.items():
             t[name_map[k[0]]][name_map[k[1]]] = c
-        with open(args.output, 'w') as f:
-            f.write("vs," + ",".join(names) + ",,sum(wins)\n")
+        output_file = Path(args.output)
+        if args.tsv:
+            output_file = output_file.with_suffix(".tsv")
+        with open(output_file, 'w') as f:
+            separator = "," if not args.tsv else "\t"
+            f.write("vs" + separator + separator.join(names) + separator * 2 + "sum(wins)\n")
             for i, name in enumerate(names):
-                f.write(name + "," + ",".join([str(c) for c in t[i]]) + ",," + str(sum(t[i])) + "\n")
-            f.write(","*(len(names)+2))
-            f.write("\nsum(losses)," + ",".join([str(sum(t[i][j] for i in range(len(names)))) for j in range(len(names))])+",,\n")
+                f.write(name + separator + separator.join([str(c) for c in t[i]]) + separator * 2 + str(sum(t[i])) + "\n")
+            f.write(separator * (len(names) + 2))
+            f.write("\nsum(losses)" + separator + separator.join([str(sum(t[i][j] for i in range(len(names)))) for j in range(len(names))]) + separator * 2 + "\n")
