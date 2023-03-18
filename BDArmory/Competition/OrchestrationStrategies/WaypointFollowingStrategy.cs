c67e1d11b18a3387d2e904ba737effec06fbdc5b
Brett Ryland <b.n.ryland@gmail.com>
2023-03-18 21:51:34 +0100

    Waypoint competitions should also stop if the competition stops for any other reason.

--- BDArmory/Competition/OrchestrationStrategies/WaypointFollowingStrategy.cs
+++ BDArmory/Competition/OrchestrationStrategies/WaypointFollowingStrategy.cs
@@ -96,7 +96,7 @@ namespace BDArmory.Competition.OrchestrationStrategies
 
             // Wait for the pilots to complete the course.
             var startedAt = Planetarium.GetUniversalTime();
-            yield return new WaitWhile(() => pilots.Any(pilot => pilot != null && pilot.weaponManager != null && pilot.IsRunningWaypoints && !(pilot.vessel.Landed || pilot.vessel.Splashed)));
+            yield return new WaitWhile(() => BDACompetitionMode.Instance.competitionIsActive && pilots.Any(pilot => pilot != null && pilot.weaponManager != null && pilot.IsRunningWaypoints && !(pilot.vessel.Landed || pilot.vessel.Splashed)));
             var endedAt = Planetarium.GetUniversalTime();
 
             BDACompetitionMode.Instance.competitionStatus.Add("Waypoints competition finished. Scores:");
