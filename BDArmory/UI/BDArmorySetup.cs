f682666ae84212215498051958dbf76da37e8e7a
BillNyeTheIE <BillNyeTheIlluminatiEye123@protonmail.com>
2023-05-21 01:38:34 -0700

    Updated Changelog and also removed cursor when turrets are GPS slaved.

--- BDArmory/UI/BDArmorySetup.cs
+++ BDArmory/UI/BDArmorySetup.cs
@@ -779,7 +779,7 @@ namespace BDArmory.UI
                             ActiveWeaponManager.selectedWeapon.GetWeaponClass() == WeaponClasses.DefenseLaser)
                         {
                             ModuleWeapon mw = ActiveWeaponManager.selectedWeapon.GetPart().FindModuleImplementing<ModuleWeapon>();
-                            if (mw != null && mw.weaponState == ModuleWeapon.WeaponStates.Enabled && mw.maxPitch > 1 && !mw.slaved && !mw.aiControlled)
+                            if (mw != null && mw.weaponState == ModuleWeapon.WeaponStates.Enabled && mw.maxPitch > 1 && !mw.slaved && !mw.GPSTarget && !mw.aiControlled)
                             {
                                 //Screen.showCursor = false;
                                 Cursor.visible = false;
