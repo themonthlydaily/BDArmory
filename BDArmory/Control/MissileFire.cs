cadf4fd5ba04af1f031f7ce72596f5792a497dfe
BillNyeTheIE <BillNyeTheIlluminatiEye123@protonmail.com>
2023-06-06 15:21:37 -0700

    SARH Launch Condition Update Fixes
    - Use .Count instead of .Count()
    - Remove micro-optimization for readability

--- BDArmory/Control/MissileFire.cs
+++ BDArmory/Control/MissileFire.cs
@@ -6331,7 +6331,7 @@ namespace BDArmory.Control
 
                                 if (firedMissiles < maxMissilesOnTarget)
                                 {
-                                    if (CurrentMissile.TargetingMode == MissileBase.TargetingModes.Radar && _radarsEnabled && !CurrentMissile.radarLOAL && (MaxradarLocks < vesselRadarData.GetLockedTargets().Count() + 1))
+                                    if (CurrentMissile.TargetingMode == MissileBase.TargetingModes.Radar && _radarsEnabled && !CurrentMissile.radarLOAL && MaxradarLocks <= vesselRadarData.GetLockedTargets().Count)
                                     {
                                         launchAuthorized = false; //don't fire SARH if radar can't support the needed radar lock
                                         if (BDArmorySettings.DEBUG_MISSILES) Debug.Log("[BDArmory.MissileFire]: radar lock number exceeded to launch!");
