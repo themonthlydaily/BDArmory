d9467401f1b7cb8fed5d6fb2c1a2d1c79c9e1c04
SuicidalInsanity <origamimage@gmail.com>
2023-08-11 15:34:04 -0700

    Missile-based LaunchAuth
    have missile launch Y/N be based on missile orientation, not vessel orientation for VLS, etc.

--- BDArmory/Control/MissileFire.cs
+++ BDArmory/Control/MissileFire.cs
@@ -6882,7 +6882,7 @@ namespace BDArmory.Control
                     float fTime = Mathf.Min(missile.dropTime, 2f);
                     Vector3 futurePos = target + (targetV.Velocity() * fTime);
                     Vector3 myFuturePos = vessel.ReferenceTransform.position + (vessel.Velocity() * fTime);
-                    launchAuthorized = launchAuthorized && (Vector3.Angle(vessel.ReferenceTransform.up, futurePos - myFuturePos) < (unguidedWeapon ? 5 : missile.maxOffBoresight * boresightFactor)); // Launch is likely also possible at fTime
+                    launchAuthorized = launchAuthorized && (Vector3.Angle(missile.GetForwardTransform(), futurePos - myFuturePos) < (unguidedWeapon ? 5 : missile.maxOffBoresight * boresightFactor)); // Launch is likely also possible at fTime
                 }
             }
 
