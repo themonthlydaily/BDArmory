b990aafa66da0ad0345a492722291d57f80eb36e
Brett Ryland <b.n.ryland@gmail.com>
2023-08-25 10:50:08 +0200

    Lower default trajectory sim multiplier to improve accuracy at extreme ranges.

--- BDArmory/Settings/BDArmorySettings.cs
+++ BDArmory/Settings/BDArmorySettings.cs
@@ -136,7 +136,7 @@ namespace BDArmory.Settings
         [BDAPersistentSettingsField] public static bool LOGARITHMIC_RADAR_DISPLAY = true;                //NOTE: used ONLY for display range of radar windows! Actual radar range provided by part configs!
         [BDAPersistentSettingsField] public static float MAX_ENGAGEMENT_RANGE = 200000f;          //NOTE: used ONLY for missile dlz parameters!
         [BDAPersistentSettingsField] public static float IVA_LOWPASS_FREQ = 2500f;
-        [BDAPersistentSettingsField] public static float BALLISTIC_TRAJECTORY_SIMULATION_MULTIPLIER = 256f;      // Multiplier of fixedDeltaTime for the large scale steps of ballistic trajectory simulations.
+        [BDAPersistentSettingsField] public static float BALLISTIC_TRAJECTORY_SIMULATION_MULTIPLIER = 128f;      // Multiplier of fixedDeltaTime for the large scale steps of ballistic trajectory simulations. Large values at extreme ranges may cause small inaccuracies. 128 with 1km/s bullets at their max range seems reasonable in most cases.
         [BDAPersistentSettingsField] public static float FIRE_RATE_OVERRIDE = 10f;
         [BDAPersistentSettingsField] public static float FIRE_RATE_OVERRIDE_CENTER = 20f;
         [BDAPersistentSettingsField] public static float FIRE_RATE_OVERRIDE_SPREAD = 5f;
