f757879608c7e87fcc84177f91a4470a77c488cd
SuicidalInsanity <origamimage@gmail.com>
2023-09-30 22:37:55 -0700

    Fix NRE with null WM

--- BDArmory/Radar/ModuleRadar.cs
+++ BDArmory/Radar/ModuleRadar.cs
@@ -955,8 +955,11 @@ namespace BDArmory.Radar
 
         public void RefreshLockArray()
         {
+			if (wpmr != null)
+			{
             attemptedLocks = new TargetSignatureData[wpmr.MaxradarLocks];
             TargetSignatureData.ResetTSDArray(ref attemptedLocks);
+			}
         }
 
         void SlaveTurrets()
