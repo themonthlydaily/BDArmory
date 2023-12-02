04e13cdf375521a93606fcde44429f28daad1f3a
josuenos <josuenos@users.noreply.github.com>
2023-12-02 15:54:48 -0500

    Formatting

--- BDArmory/Control/MissileFire.cs
+++ BDArmory/Control/MissileFire.cs
@@ -7732,7 +7732,7 @@ namespace BDArmory.Control
                 {
                     target = MissileGuidance.GetAirToAirFireSolution(missile, targetV);
                 }
-                
+
                 float boresightFactor = (mf.vessel.LandedOrSplashed || targetV.LandedOrSplashed || missile.uncagedLock) ? 0.75f : 0.35f; // Allow launch at close to maxOffBoresight for ground targets or missiles with allAspect = true
 
                 //if(missile.TargetingMode == MissileBase.TargetingModes.Gps) maxOffBoresight = 45;
