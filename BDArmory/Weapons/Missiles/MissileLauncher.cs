0e8191a6f9ef041d851ce0097d4c4c76fbc23d09
josuenos <josuenos@users.noreply.github.com>
2024-01-13 12:05:00 -0500

    - Fix for issue #596 (bombs exploding midair)

--- BDArmory/Weapons/Missiles/MissileLauncher.cs
+++ BDArmory/Weapons/Missiles/MissileLauncher.cs
@@ -1539,6 +1539,7 @@ namespace BDArmory.Weapons.Missiles
 
         private void CheckMiss()
         {
+            if (weaponClass == WeaponClasses.Bomb) return;
             float sqrDist = (float)((TargetPosition + (TargetVelocity * Time.fixedDeltaTime)) - (vessel.CoM + (vessel.Velocity() * Time.fixedDeltaTime))).sqrMagnitude;
             bool targetBehindMissile = !TargetAcquired || (!(MissileState != MissileStates.PostThrust && hasRCS) && Vector3.Dot(TargetPosition - transform.position, transform.forward) < 0f); // Target is not acquired or we are behind it and not an RCS missile
             if (sqrDist < 160000 || MissileState == MissileStates.PostThrust || (targetBehindMissile && sqrDist > 1000000)) //missile has come within 400m, is post thrust, or > 1km behind target
