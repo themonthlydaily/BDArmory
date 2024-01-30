2f09292e810e668452ed5154541f55125907c1fd
SuicidalInsanity <origamimage@gmail.com>
2024-01-30 15:08:07 -0800

    Update BDArmorySetup.cs
    re-add missing guns, fix name mismatch with N-37/Nudelman

--- BDArmory/UI/BDArmorySetup.cs
+++ BDArmory/UI/BDArmorySetup.cs
@@ -3442,7 +3442,7 @@ namespace BDArmory.UI
                         {
                             MutatorInfo.SetupGunGame();
                             BDArmorySettings.MUTATOR_LIST.Clear();
-                            BDArmorySettings.MUTATOR_LIST = new List<string> { "Chainguns", "Vulcans", "Abrams", "Mausers", "GAU-22s", "Nudelmans", "Rocket Arena", "Railguns", "GAU-8s"}; //generally weaker to stronger, but with early abrams to give pooer performing planes a potential leg up
+                            BDArmorySettings.MUTATOR_LIST = new List<string> { "Brownings", "Chainguns", "Vulcans", "Abrams", "Mausers", "GAU-22s", "N-37s", "Rocket Arena", "AT Guns", "Railguns", "GAU-8s"}; //generally weaker to stronger, but with early abrams to give poorer performing planes a potential leg up
                             //BDArmorySettings.MUTATOR_LIST = new List<string> { "GAU-8s", "Railguns", "Abrams", "Rocket Arena", "Nudelmans", "GAU-22s", "Mausers", "Vulcans", "Chainguns" }; //reverse gunGame, in case of runaway rich-get-richer scenario
                             //as long as game is started on RWP_Round == 61, this should be editable in the settings.cfg to change it to whatever order is needed for the showrunner
                         }
