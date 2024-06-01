e9d8f6ad2bab3bd9ec3feb395e6dcf2c7256386c
BillNyeTheIE <BillNyeTheIlluminatiEye123@protonmail.com>
2024-06-01 15:39:42 -0700

    Fix AirToAirFireSolution Issue
    - Correct error that occurred during a merge

--- BDArmory/Guidances/MissileGuidance.cs
+++ BDArmory/Guidances/MissileGuidance.cs
@@ -722,7 +722,12 @@ namespace BDArmory.Guidances
             }
             else // Not accurate enough for orbital speeds, but more resilient to acceleration changes
             {
-                targetPosition += (Vector3)targetVessel.acceleration_immediate * 0.05f * leadTime * leadTime;
+                targetPosition += leadTime * targetVessel.Velocity();
+
+                if (targetVessel && targetDistance < 800) //TODO - investigate if this would throw off aim accuracy
+                {
+                    targetPosition += (Vector3)targetVessel.acceleration * 0.05f * leadTime * leadTime;
+                }
             }
 
             return targetPosition;
