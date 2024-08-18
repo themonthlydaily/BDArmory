9066194a2104c4530e41b6c01bfced69875f520c
SuicidalInsanity <origamimage@gmail.com>
2024-08-18 11:41:59 -0700

    Update ModuleWeapon.cs

--- BDArmory/Weapons/ModuleWeapon.cs
+++ BDArmory/Weapons/ModuleWeapon.cs
@@ -6190,7 +6190,7 @@ namespace BDArmory.Weapons
                 {
                     output.AppendLine($"Rounds Per Minute: {roundsPerMinute * (fireTransforms?.Length ?? 1)}");
                     if (SpoolUpTime > 0) output.AppendLine($"Weapon requires {SpoolUpTime} seconds to come to max RPM");
-                    output.AppendLine($"Shot Deviation: {Mathf.Sin(maxDeviation / 57.29577f) * 1000:F2} mrad");
+                    output.AppendLine($"Shot Deviation: {(((Mathf.Sin(maxDeviation / 57.29577f) * 1000) / 2) * 1.285f) * 2:F2} mrad, 80% hit");
                     if (HEpulses)
                     {
                         output.AppendLine($"Blast:");
@@ -6217,7 +6217,7 @@ namespace BDArmory.Weapons
                 }
                 if (weaponType == "ballistic")
                 {
-                    output.AppendLine($"Shot Deviation: {Mathf.Sin(maxDeviation / 57.29577f) * 1000:F2} mrad");
+                    output.AppendLine($"Shot Deviation: {(((Mathf.Sin(maxDeviation / 57.29577f) * 1000) / 2) * 1.285f) * 2:F2} mrad, 80% hit");
                     for (int i = 0; i < ammoList.Count; i++)
                     {
                         BulletInfo binfo = BulletInfo.bullets[ammoList[i].ToString()];
