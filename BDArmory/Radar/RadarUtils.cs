3f5ddfdce7733dadf0ac41f7f658fc6a88a02986
josuenos <67711422+josuenos@users.noreply.github.com>
2024-08-24 19:38:09 -0400

    Null checks for missile teammate condition

--- BDArmory/Radar/RadarUtils.cs
+++ BDArmory/Radar/RadarUtils.cs
@@ -2093,7 +2093,9 @@ namespace BDArmory.Radar
             {
                 Vector3 relV = missile.vessel.Velocity() - mf.vessel.Velocity();
                 bool approaching = Vector3.Dot(relV, vectorFromMissile) > 0;
-                bool teammate = (VesselModuleRegistry.GetMissileFire(missile.SourceVessel).team == mf.team) && (missile.targetVessel != null ? missile.targetVessel != mf.vessel : true); // Missile is fired from teammate and not locked onto us
+                bool teammate = false; // Missile isn't coming from teammate
+                if (missile.SourceVessel != null && VesselModuleRegistry.GetMissileFire(missile.SourceVessel) != null)
+                    teammate = (VesselModuleRegistry.GetMissileFire(missile.SourceVessel).team == mf.team) && (missile.targetVessel != null ? missile.targetVessel != mf.vessel : true); // Missile is fired from teammate and not locked onto us
                 bool withinRadarFOV = (missile.TargetingMode == MissileBase.TargetingModes.Radar && !teammate) ?
                     (Vector3.Angle(missile.GetForwardTransform(), vectorFromMissile) <= Mathf.Clamp(missile.lockedSensorFOV, 40f, 90f) / 2f) : false;
                 var missileBlastRadiusSqr = teammate ? mf.vessel.GetRadius() : 3f * Mathf.Max(missile.GetBlastRadius(), mf.vessel.GetRadius()); // Blast radius or self radius, whichever is larger (use self radius if missile is from teammate)
