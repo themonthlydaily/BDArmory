d8ea80e0c01f3fc27bdbd77bc91f84606a754ed8
Brett Ryland <b.n.ryland@gmail.com>
2024-10-08 23:21:04 +0200

    Use the right UI_SCALE for the settings window.

--- BDArmory/UI/BDArmorySetup.cs
+++ BDArmory/UI/BDArmorySetup.cs
@@ -924,7 +924,7 @@ namespace BDArmory.UI
 
             if (!windowBDAToolBarEnabled || !HighLogic.LoadedSceneIsFlight) return;
             SetGUIOpacity();
-            if (BDArmorySettings.UI_SCALE != 1) GUIUtility.ScaleAroundPivot(BDArmorySettings.UI_SCALE * Vector2.one, WindowRectToolbar.position);
+            if (BDArmorySettings._UI_SCALE != 1) GUIUtility.ScaleAroundPivot(BDArmorySettings._UI_SCALE * Vector2.one, WindowRectToolbar.position);
             WindowRectToolbar = GUI.Window(321, WindowRectToolbar, WindowBDAToolbar, "", BDGuiSkin.window);//"BDA Weapon Manager"
             SetGUIOpacity(false);
             GUIUtils.UseMouseEventInRect(WindowRectToolbar);
